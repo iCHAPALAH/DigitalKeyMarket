5ae337820b25a7e4e22245a36aa1f4d5e1a0dda1
iCHAPALAH <139129056+iCHAPALAH@users.noreply.github.com>
2024-10-29 20:32:43 +0300

    Finished lab2

---
+++ DigitalKeyMarket.Service/IoC/SwaggerConfigurator.cs
@@ -0,0 +1,16 @@
+﻿namespace DigitalKeyMarket.Service.IoC;
+
+public static class SwaggerConfigurator
+{
+    public static void ConfigureServices(WebApplicationBuilder builder)
+    {
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+    }
+    
+    public static void ConfigureApplication(IApplicationBuilder app)
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+}
