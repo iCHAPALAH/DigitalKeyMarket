6b2695304517c1d2971091ecd8fb66391c956ac7
iCHAPALAH <139129056+iCHAPALAH@users.noreply.github.com>
2024-11-04 00:15:03 +0300

    Finished Lab3

--- DigitalKeyMarket.DataAccess/Entities/ItemEntity.cs
+++ DigitalKeyMarket.DataAccess/Entities/ItemEntity.cs
@@ -8,13 +8,13 @@ public class ItemEntity : BaseEntity
     public int Stock { get; set; }
     public int Price { get; set; }
     
-    public Guid GameId { get; set; }
+    public int GameId { get; set; }
     public GameEntity Game { get; set; }
     
-    public Guid PlaygateId { get; set; }
+    public int PlaygateId { get; set; }
     public PlaygateEntity Playgate { get; set; }
     
-    public Guid EditionId { get; set; }
+    public int EditionId { get; set; }
     public EditionEntity Edition { get; set; }
     
     public List<PurchaseEntity> Users { get; set; }
